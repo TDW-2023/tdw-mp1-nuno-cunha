a2767ad0b89465f06e38d6d6e053456fb8c39666
Nuno Cunha <nunocunha@ua.pt>
2023-10-19 13:54:51 +0100

    Prettier fix

--- components/alert.test.js
+++ components/alert.test.js
@@ -1,11 +1,9 @@
-import { render, screen } from '@testing-library/react'
-import Alert from './alert'
+import { render, screen } from "@testing-library/react";
+import Alert from "./alert";
 
 test('Component with "components/alert.js" should exist', async () => {
+  render(<Alert />);
 
-    render(<Alert />);
-    
-    const element = document.createElement('div');
-    expect(element).not.toBeNull();
-
+  const element = document.createElement("div");
+  expect(element).not.toBeNull();
 });
--- jest.config.js
+++ jest.config.js
@@ -196,5 +196,5 @@ const config = {
 };
 
 module.exports = {
-    "testEnvironment": "jsdom"
+  testEnvironment: "jsdom",
 };
